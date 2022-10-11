0e2fa1f51d738d468141931f41aaa1c4c9f0ffe1
Omar Mohamed <omvr94@gmail.com>
2022-10-11 09:04:59 -0600

    changed sqlite cars db, api end point for adding branch and cartype is complete. Added a car to a branch.

--- car_rental/serializers.py
+++ car_rental/serializers.py
@@ -1,8 +1,20 @@
 #Serializers = converts from python object to JSON
 from rest_framework import serializers
-from .models import Car
+from .models import Car, CarType, Branch
 
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
-        fields = ['id','manufacturer','model','fuelType','colour','licensePlate','status','mileage']
+        fields = ['id','manufacturer','model','fuelType','colour','licensePlate','status','mileage', 'typeID', 'BranchID']
+
+
+class CarTypeSerilzer(serializers.ModelSerializer):
+    class Meta:
+        model = CarType
+        fields = ['id', 'description', 'dailyCost', 'weeklyCost', 'monthlyCost', 'lateFee', 'changeBranchFee']
+
+
+class BranchSerilzer(serializers.ModelSerializer):
+    class Meta:
+        model = Branch
+        fields = ["id", "phoneNumber", "province", "city", "postalCode", "streetNumber", "streetName", "unitNumber"]
--- car_rental/urls.py
+++ car_rental/urls.py
@@ -21,7 +21,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cars/', views.car_list),
-    path('cars/<int:id>', views.car_detail)
+    path('cars/<int:id>', views.car_detail),
+    path('carType/', views.car_type),
+    path('branch/', views.branch_details),
+    
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
