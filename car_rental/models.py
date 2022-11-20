9cbdf5b9fa5da129715057b5bb218e92b9c93fae
songw5 <songw5@mymacewan.ca>
2022-11-20 12:33:58 -0700

    Rental table fixes for Ryan

--- car_rental/models.py
+++ car_rental/models.py
@@ -1,16 +1,18 @@
 from unittest.util import _MAX_LENGTH
 from django.db import models
 
+
 class Branch(models.Model):
     id = models.AutoField(primary_key=True)
-    branchName = models.CharField(max_length=50,null=True,blank=True)
+    branchName = models.CharField(max_length=50, null=True, blank=True)
     phoneNumber = models.CharField(max_length=12)
     province = models.CharField(max_length=45)
     city = models.CharField(max_length=45)
     postalCode = models.CharField(max_length=7)
     streetNumber = models.CharField(max_length=45)
     streetName = models.CharField(max_length=45)
-    unitNumber = models.IntegerField(null=True,blank=True)
+    unitNumber = models.IntegerField(null=True, blank=True)
+
 
 class CarType(models.Model):
     id = models.AutoField(primary_key=True)
@@ -20,6 +22,7 @@ class CarType(models.Model):
     monthlyCost = models.FloatField()
     lateFee = models.FloatField()
     changeBranchFee = models.FloatField()
+
 
 class Car(models.Model):
     id = models.AutoField(primary_key=True)
@@ -32,10 +35,12 @@ class Car(models.Model):
     mileage = models.IntegerField()
     typeID = models.ForeignKey(CarType, on_delete=models.CASCADE)
     BranchID = models.ForeignKey(Branch, on_delete=models.CASCADE)
-    image = models.ImageField(default="",upload_to="./car_pictures", null=True, blank=True)
+    image = models.ImageField(
+        default="", upload_to="./car_pictures", null=True, blank=True)
 
     def __str__(self):
-        return self.manufacturer+ ' ' +self.model + ' - ' + str(self.mileage)
+        return self.manufacturer + ' ' + self.model + ' - ' + str(self.mileage)
+
 
 class Customer(models.Model):
     id = models.AutoField(primary_key=True)
@@ -45,45 +50,49 @@ class Customer(models.Model):
     email = models.EmailField()
     password = models.CharField(max_length=100)
     phoneNumber = models.CharField(max_length=45)
-    dob = models.DateField() #https://www.geeksforgeeks.org/datefield-django-models/
-    goldMembership = models.CharField(blank=True, max_length=45)
+    dob = models.DateField()  # https://www.geeksforgeeks.org/datefield-django-models/
+    goldMembership = models.BooleanField(null=True, blank=True)
+    city = models.CharField(max_length=45)
     province = models.CharField(max_length=45)
     postalCode = models.CharField(max_length=7)
     streetNumber = models.CharField(max_length=45)
     streetName = models.CharField(max_length=45)
     unitNumber = models.IntegerField(null=True, blank=True)
 
+
 class Employee(models.Model):
     id = models.AutoField(primary_key=True)
     firstname = models.CharField(max_length=45)
     lastname = models.CharField(max_length=45)
-    email = models.EmailField(null=True,blank=True)
+    email = models.EmailField(null=True, blank=True)
     phoneNumber = models.CharField(max_length=45)
     password = models.CharField(max_length=100)
     province = models.CharField(max_length=45)
     postalCode = models.CharField(max_length=7)
     streetNumber = models.CharField(max_length=45)
     streetName = models.CharField(max_length=45)
-    unitNumber = models.IntegerField(null=True,blank=True)
+    unitNumber = models.IntegerField(null=True, blank=True)
     branchID = models.ForeignKey(Branch, on_delete=models.CASCADE)
+
 
 class Rental(models.Model):
     id = models.AutoField(primary_key=True)
     dateFrom = models.DateField()
     dateTo = models.DateField()
-    dateReturned = models.DateField(null=True,blank=True)
-    totalCost = models.FloatField(null=True,blank=True)
-    licensePlate = models.CharField(max_length=45)
-    
-    #How to pull from customer.goldMembership?
-    goldMembership = models.CharField(blank=True,max_length=45) #incorrect rn
-    
-    #Foreign Key
-    branchID = models.ForeignKey(Branch, on_delete=models.CASCADE)
+    dateReturned = models.DateField(null=True, blank=True)
+    totalCost = models.FloatField(null=True, blank=True)
+    # Foreign Key
+    rentalBranchID = models.ForeignKey(Branch, on_delete=models.CASCADE)
+    returnBranchID = models.ForeignKey(
+        Branch, on_delete=models.CASCADE, null=True, blank=True)
     carID = models.ForeignKey(Car, on_delete=models.CASCADE)
-    typeID = models.ForeignKey(CarType,on_delete=models.CASCADE)
+    typeID = models.ForeignKey(CarType, on_delete=models.CASCADE)
     customerID = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    employeeID = models.ForeignKey(Employee, on_delete=models.SET_NULL, null=True, blank=True)
+    rentalEmployeeID = models.ForeignKey(
+        Employee, on_delete=models.SET_NULL, null=True, blank=True)
+    returnEmployeeID = models.ForeignKey(
+        Employee, on_delete=models.SET_NULL, null=True, blank=True)
+
 
 class CarDamage(models.Model):
     id = models.AutoField(primary_key=True)
@@ -91,4 +100,5 @@ class CarDamage(models.Model):
     description = models.TextField()
     damageCost = models.IntegerField()
     carID = models.ForeignKey(Car, on_delete=models.CASCADE)
-    image = models.ImageField(default="",upload_to="./car_pictures/car_damages")
+    image = models.ImageField(
+        default="", upload_to="./car_pictures/car_damages")
