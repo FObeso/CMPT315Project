954870a27305b496936c97df85066b0e3e6a0521
Omar Mohamed <mohamedo5@mymacewan.ca>
2022-11-24 18:16:00 -0700

    added carinfo compoenent in page

---
+++ frontend/src/pages/customer/RentConfirmation.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import CarInfo from "../../components/CarInfo"
+
+const RentConfirmation = () => {
+
+  return <div> <CarInfo/>
+  
+  
+  </div>;
+};
+
+
+export default RentConfirmation;
