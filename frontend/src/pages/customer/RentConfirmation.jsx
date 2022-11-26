2f7bc60ecfb86c6b26cd4630359ff6fd104a2729
Omar Mohamed <mohamedo5@mymacewan.ca>
2022-11-25 23:01:31 -0700

    example of proxy data, client can pass in different car id's to see info

--- frontend/src/pages/customer/RentConfirmation.jsx
+++ frontend/src/pages/customer/RentConfirmation.jsx
@@ -3,10 +3,31 @@ import CarInfo from "../../components/CarInfo"
 
 const RentConfirmation = () => {
 
-  return <div> <CarInfo/>
+  /*Must pass in the following props in order for the page to display
+  car all information and for the post request to the db to be successful 
   
+   car_id
+   customer_id
+   rental_cost
+   rental_start_date
+   rental_end_date  */
+   
+
+  return <div>  
   
-  </div>;
+  <CarInfo
+  
+  car_id={2} 
+  rental_cost={102} 
+  rental_start_date={"2022-11-21"} 
+  rental_end_date={"2022-17-21"} 
+  customer_id ={1} 
+  
+  />
+    </div>;
+
+
+
 };
 
 
